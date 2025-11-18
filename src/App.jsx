e7b0ffd08c950097c3069df89f944750898e7909
ruby-cs <18ruby.cs@gmail.com>
2025-11-17 22:03:49 -0800

    adding leaderboard to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import {Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import Home from './pages/Home.jsx';
 import Profile from './pages/Profile.jsx';
+import Leaderboard from "./pages/Leaderboard.jsx";
 
 const App = () => (
     <div>
         <Navbar />
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/leaderboard" element={<Leaderboard />} />
             <Route path="/profile" element={<Profile />} />
         </Routes>
     </div>
